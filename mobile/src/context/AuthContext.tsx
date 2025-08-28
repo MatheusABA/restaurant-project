5d8740a3191a38a92bd4b3af45bf17be17321497
MatheusABA <matheus.alencar02199@gmail.com>
2025-08-28 14:06:20 -0400

    [new]: Initial mobile

---
+++ mobile/src/context/AuthContext.tsx
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+export interface AuthContextType {
+  token: string;
+  user: { id: number; name: string; email: string; role: string } | null;
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextType>({
+  token: "",
+  user: null,
+  login: () => {},
+  logout: () => {},
+});
